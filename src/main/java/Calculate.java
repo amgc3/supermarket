942383a68e57ce34a3feb2f28dc9baa41f6e69f9
amgc3 <anna.cotugno@deltacapita.com>
2022-03-09 16:01:56 +0000

    remove redundant type

--- src/main/java/Calculate.java
+++ src/main/java/Calculate.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 public class Calculate {
 
     public static HashMap<String, Integer> sortBasket(String[] items) {
-        HashMap<String, Integer> itemMap = new HashMap<String, Integer>();
+        HashMap<String, Integer> itemMap = new HashMap<>();
         for (String item : items) {
             if (itemMap.containsKey(item)) {
                 itemMap.put(item, itemMap.get(item) + 1);
@@ -21,7 +21,6 @@ public class Calculate {
             // Melons are 50p each, but are available as ‘buy one get one free’
             if (item == "Melon" && itemMap.get(item) >= 2) {
                 total = total.add(new BigDecimal(Integer.toString((itemMap.get(item) / 2) + (itemMap.get(item) % 2))).multiply(priceMap.get(item)));
-
             // Limes are 15p each, but are available in a ‘three for the price of two’ offer
             } else if (item == "Lime" && itemMap.get(item) >= 3) {
                 total = total.add(new BigDecimal(Integer.toString((itemMap.get(item) / 3) * 2 + (itemMap.get(item) % 3))).multiply(priceMap.get(item)));
@@ -35,7 +34,7 @@ public class Calculate {
 
     public static void main(String[] args) {
         // hashmap of items and prices
-        HashMap<String, BigDecimal> itemPriceMap = new HashMap<String, BigDecimal>();
+        HashMap<String, BigDecimal> itemPriceMap = new HashMap<>();
         itemPriceMap.put("Banana", new BigDecimal(Double.toString(0.20)));
         itemPriceMap.put("Apple", new BigDecimal(Double.toString(0.35)));
         itemPriceMap.put("Melon", new BigDecimal(Double.toString(0.50)));
