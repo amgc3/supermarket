a19530c8b0653b27ba16a77d1d07aa6d01f457e4
amgc3 <anna.cotugno@deltacapita.com>
2022-04-05 18:16:26 +0100

    begin of calculate class tests

--- src/test/java/CalculateTest.java
+++ src/test/java/CalculateTest.java
@@ -1,15 +1,43 @@
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static priceClass.Calculate3.sortBasket;
+
 public class CalculateTest {
 
     @Test
-    @DisplayName("sort basket")
-    void sortBasket() {
+    @DisplayName("returns frequency table correct size")
+    void testSortBasketSize() {
+        String[] items = {"Banana", "Apple", "Banana", "Apple", "Banana", "Melon", "Melon", "Melon", "Lime", "Apple"};
+        Map<String, Integer> map = new HashMap<>();
+        map.put("Banana", 3);
+        map.put("lime", 1);
+        map.put("Apple", 3);
+        map.put("Melon", 3);
+        Map<String, Integer> actual = sortBasket(items);
+        assertEquals(map.size(), actual.size());
     }
 
     @Test
-    @DisplayName("get total")
+    @DisplayName("return frequency table")
+    void testSortBasket() {
+        String[] items = {"Banana", "Apple", "Banana", "Apple", "Banana", "Melon", "Melon", "Melon", "Lime", "Apple"};
+        HashMap<String, Integer> expected = new HashMap<>();
+        expected.put("Banana", 3);
+        expected.put("Lime", 1);
+        expected.put("Apple", 3);
+        expected.put("Melon", 3);
+        Map<String, Integer> actual = sortBasket(items);
+        assertTrue(expected.equals(actual));
+    }
+
+    @Test
+    @DisplayName("calculate total")
     void getTotal() {
     }
 }
